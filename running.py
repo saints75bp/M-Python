d3f5afa49e9e3e50f0350405bcc30abd38e124d0
6022792970 <6022792970@g.siit.tu.ac.th>
2024-07-08 09:25:31 +0700

    first commit

---
+++ running.py
@@ -0,0 +1,11 @@
+active99 = input("enter status\n>>");
+active99 = int(active99);
+
+if active99 >= 1 and active99 <= 5:
+    print("waiting for your work!!!!")
+elif active99 == 99 or active99 == 98:
+    print("cry alot");
+elif not(active99 > 51):
+    print("vaccation");
+else:
+    print("we will search for the code meaning");
